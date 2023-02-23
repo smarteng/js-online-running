60b704767fb963dd8a9e2d327c0e020d4e2d68e6
jerryteng <jerryteng@tencent.com>
2023-02-23 15:52:44 +0800

    up

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'js在线编辑-运行平台' });
+  res.render('index', { title: 'js在线编辑' });
 });
 
 
